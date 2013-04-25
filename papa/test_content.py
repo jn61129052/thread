7172614fdc526ad0462cc6cac65e13001b471eff
jiangnan <jn61129052@163.com>
2013-04-25 17:22:13 +0800

    4.15
    依然不能正确获取Sina的网页编码……我日

--- papa/test_content.py
+++ papa/test_content.py
@@ -6,33 +6,32 @@ Created on 2013-4-24
 import re
 import urllib2
 from bs4 import BeautifulSoup
-# def get_response_charset(charset):
-#     if charset == 'utf-8':
-#         return charset
-#     elif charset == 'gb2312' or charset == 'GBK':
-#         charset = 'GBK'
-#         return charset
-# def get_charset(url):      #Determine the URL-encoded
-#     fopen1 = urllib.urlopen(url)
-#     charset = fopen1.info().getparam('charset')
-#     if charset:
-#         charset = get_response_charset(charset)
-#         return charset
-#     else:
-#         charset = fopen1.headers['Content-Type'].split(' charset=')[1].lower()
-#         charset = get_response_charset(charset)
-#         return charset
-def work(url):
-    try:
-        html = urllib2.urlopen(url).read()
-    except UnicodeError,e:
-        return None
-    except Exception,e:
-        return None
-    soup = BeautifulSoup(html)
-    links = soup.find('a')
-    return links
+def get_charset(url):      #Determine the URL-encoded
+    fopen1 = urllib2.urlopen(url)
+    charset_html = fopen1.headers['Content-Type'].split(' charset=')[1].lower()
+    print charset_html
+    charset = fopen1.info().getparam('charset')
+    print charset
+    if charset is None:
+        charset = get_html_charset(fopen1)
+        return charset
+    else:
+        charset = get_response_charset(charset)
+        return charset
+def get_html_charset(html):
+    charset_html = html.headers['Content-Type'].split(' charset=')[1].lower()
+    print charset_html
+    charset = get_response_charset(charset_html)
+    return charset
+def get_response_charset(charset):
+    charset = charset.lower()
+    print charset
+    if charset == 'utf-8':
+        return charset
+    elif charset == 'gb2312' or charset == 'gbk':
+        charset = 'gbk'
+        return charset
 if __name__ == '__main__':
-    url = "https://github.com/"
-    print work(url)
+    url = "http://www.sina.com.cn/"
+    print get_charset(url)
     
