55b91905a397335e88b336f254dc28c8e462d68f
jiangnan <jn61129052@163.com>
2013-05-08 15:12:51 +0800

    目前还有一些BUG 1 抓取到得某些连接301跳转以后是图片，然后程序就不动了 2 有部分乱码出现

--- papa/sina_papa.py
+++ papa/sina_papa.py
@@ -13,48 +13,70 @@ from string import strip
 #import chardet
 import requests
 start = time.clock()
-url = "http://www.sina.com.cn/"  # must add "http://"
-htmlline = requests.get(url)
-def get_response_charset(charset):   
-    if charset.lower() == 'utf-8':
-        return  'utf-8'
-    elif charset.lower() == 'gb2312' or charset.lower() == 'gbk' or charset.lower() == 'iso-8859-1':
-        return 'gb18030'
-htmlline.encoding = get_response_charset(htmlline.encoding)
-soup = BeautifulSoup(htmlline.text.decode(htmlline.encoding,'ignore'))  
-links = soup.findAll('a')
-regex = re.compile(r'([A-z0-9]+[_\-]?[A-z0-9]+\.)*[A-z0-9]+\-?[A-z0-9]+\.[A-z]{2,}(\/.*)*\/?')  
-count = 0
-def output_file(href_link,count): 
-    file_object = open('J:/1.txt','a')
-    try:
-        for i in range(0,count):
+f = open("J://1.txt", "r")
+while True:
+    line = f.readline()
+    if line:
+        href_link = []
+        try:
+            htmlline = requests.get(line,timeout = 3)
+            print htmlline.encoding
+            print line
+            def get_response_charset(charset):   
+                if charset.lower() == 'utf-8' or charset.lower() == 'utf8':
+                    return  'utf-8'
+                elif charset.lower() == 'gb2312' or charset.lower() == 'gbk' or charset.lower() == 'iso-8859-1':
+                    return 'gb18030'
             try:
-                file_object.write(href_link[i])
+                htmlline.encoding = get_response_charset(htmlline.encoding)
+                soup = BeautifulSoup(htmlline.content.decode(htmlline.encoding,'ignore'))
             except UnicodeEncodeError,e:
+                regex1 = r'<meta.*(?:(?:charset\s*=\s*["|\']?)|(?:charset.*content\s*=\s*["|\']\s*))([\d|\w|\-]+)[;|"|\'|\s]'
+                code = re.search(regex1,htmlline.content)
+                result = code.group(1)
+                print result
                 print e
-    except IOError:
-        print "IO Error!"
-    finally:
-        file_object.close()
-href_link = []
-def character_filter(character):
-    character = character.replace(u'\xa0',' ').strip(u'\r\n').strip(u'\n')
-    return character   
-for item in links:
-    if 'href' in str(item):
-        if regex.search(str(item)):
-            linkname = item.string
-            linkaddr = item['href']
-            if linkname is not None:  #BeautifulSoup can not encode ;&nbsp so replace it to ' '
-                character_filter(linkname)
-                character_filter(linkaddr)
-                if 'NoneType' in str(type(linkname)):
-                    href_link.append(linkaddr+'\n')
-                    count += 1
-                else: 
-                    href_link.append(linkname+':'+linkaddr+'\n')
-                    count += 1
-output_file(href_link,count)
+                htmlline.encoding = result
+                htmlline.encoding = get_response_charset(htmlline.encoding)
+                print htmlline.encoding + '1111'
+                soup = BeautifulSoup(htmlline.text.decode(htmlline.encoding,'ignore'))
+            links = soup.findAll('a')
+            regex = re.compile(r'(https?|ftp|mms):\/\/([A-z0-9]+[_\-]?[A-z0-9]+\.)*[A-z0-9]+\-?[A-z0-9]+\.[A-z]{2,}(\/.*)*\/?')  
+            count = 0
+            def output_file(href_link,count): 
+                try:
+                    with open('J:/2.txt','a') as file_object:
+                        for i in range(0,count):
+                            try:
+                                file_object.write(href_link[i])
+                            except UnicodeEncodeError,e:
+                                pass
+                except IOError:
+                    print "IO Error!"
+    
+            def character_filter(character):
+                character = character.replace(u'\xa0',' ').strip(u'\r\n').strip(u'\n') #BeautifulSoup can not encode ;&nbsp so replace it to ' '
+                return character   
+            for item in links:
+                if 'href' in str(item):
+                    if regex.search(str(item)):
+                        linkname = item.string
+                        linkaddr = item['href']
+                        print linkaddr
+                        if linkname is not None:  
+                            character_filter(linkname)
+                            character_filter(linkaddr)
+                            if 'NoneType' in str(type(linkname)):
+                                href_link.append(linkaddr+'\n')
+                                count += 1
+                            else: 
+                               href_link.append(linkname+':'+linkaddr+'\n')
+                               #href_link.append(linkaddr+'\n')
+                               count += 1
+#                         else:
+#                             break
+            output_file(href_link,count)
+        except:
+            pass
 end = time.clock()
 print end-start
--- papa/test_content.py
+++ papa/test_content.py
@@ -3,15 +3,57 @@ Created on 2013-4-24
 
 @author: Administrator
 '''
-# import re
-# import urllib
-# from bs4 import BeautifulSoup
-# import chardet
-# data = urllib.urlopen('http://www.sina.com.cn').read()
-# print chardet.detect(data)['encoding']
+from bs4 import BeautifulSoup
 import requests
-url = 'http://www.sina.com.cn'
-response = requests.get(url)
-print response
-
-
+import re
+def character_filter(character):
+    character = character.replace(u'\xa0',' ').strip(u'\r\n').strip(u'\n') #BeautifulSoup can not encode ;&nbsp so replace it to ' '
+    return character  
+def get_response_charset(charset):   
+        if charset.lower() == 'utf-8' or charset.lower() == 'utf8':
+            return  'utf-8'
+        elif charset.lower() == 'gb2312' or charset.lower() == 'gbk' or charset.lower() == 'iso-8859-1':
+            return 'gb18030'
+url = 'http://www.sina.com.cn/'
+html = requests.get(url)
+#print html.headers['content-type'].split('charset=')[-1]
+regex1 = r'<meta.*(?:(?:charset\s*=\s*["|\']?)|(?:charset.*content\s*=\s*["|\']\s*))([\d|\w|\-]+)[;|"|\'|\s]'
+code = re.search(regex1,html.content)
+result = code.group(1)
+html.encoding = result
+html.encoding = get_response_charset(html.encoding)
+soup = BeautifulSoup(html.content.decode('utf-8','ignore'))
+links = soup.findAll('a')
+regex = re.compile(r'(https?|ftp|mms):\/\/([A-z0-9]+[_\-]?[A-z0-9]+\.)*[A-z0-9]+\-?[A-z0-9]+\.[A-z]{2,}(\/.*)*\/?')  
+count = 0
+def output_file(href_link,count): 
+        try:
+            with open('J:/2.txt','a') as file_object:
+                for i in range(0,count):
+                    try:
+                        file_object.write(href_link[i])
+                    except UnicodeEncodeError,e:
+                        pass
+        except IOError:
+            print "IO Error!"
+href_link = []
+def character_filter(character):
+    character = character.replace(u'\xa0',' ').strip(u'\r\n').strip(u'\n') #BeautifulSoup can not encode ;&nbsp so replace it to ' '
+    return character  
+for item in links:
+        if 'href' in str(item):
+            if regex.search(str(item)):
+                linkname = item.string
+                linkaddr = item['href']
+                if linkname is not None:  
+                    character_filter(linkname)
+                    character_filter(linkaddr)
+                    if 'NoneType' in str(type(linkname)):
+                        href_link.append(linkaddr+'\n')
+                        count += 1
+                    else: 
+                        href_link.append(linkname+':'+linkaddr+'\n')
+                        #href_link.append(linkaddr+'\n')
+                        count += 1
+output_file(href_link,count)
+print count
