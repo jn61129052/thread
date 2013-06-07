2c1480133aa82ffb2324fe6cbed052f658fe51d8
jiangnan <jn61129052@163.com>
2013-06-07 15:55:47 +0800

    重构start方法

--- papa/test.py
+++ papa/test.py
@@ -16,6 +16,9 @@ import os
 import hashlib
 import Queue
 import threading
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 # 
 # urlQueue = Queue.Queue()
 # 
@@ -40,23 +43,16 @@ class Crawler(object):
         #当前爬行深度
         self.currentdepth = 1
         #已经访问的链接
-#         self.href_link = []
+        self.currenturls = []
         #经过处理的链接
-        self.geturls = []
-        #创建hash表，判断是否
+        self.result = []
+        #创建hash表，判断是否已经爬取
         self.hashmap = []
         #数据库存储文件
-        self.result = []
         self.dbfile = Dbfile
         #创建数据库处理对象
         self.database = DataBase(self.dbfile)
         
-    #编码规则化处理
-    def get_response_charset(self,charset):
-        if charset.lower() == 'utf-8' or charset.lower == 'utf8':
-            return  'utf-8'
-        elif charset.lower() == 'gb2312' or charset.lower() == 'gbk' or charset.lower() == 'iso-8859-1':
-            return 'gb2312'
     #BeautifulSoup 无法正确处理;&nbsp这类的字符，过滤处理类
     def character_filter(self,character):
         character = character.replace(u'\xa0',' ').strip(u'\r\n').strip(u'\n') #BeautifulSoup can not encode ;&nbsp so replace it to ' '
@@ -64,62 +60,33 @@ class Crawler(object):
     #数据库关闭类
     def conn_close(self):
         self.database.conn_stop()
-     
-    #list去重，空间复杂度过大，pass
-#     def unique(self,old_list):
-#         return list(set(old_list))
-
 
     #MD5处理url，用于去重
-    def md5(self,urls):       
+    def md5(self,urls):
         return hashlib.new("md5", urls).hexdigest()
+    
+    #http头检测
+    def http(self,url):
+        if not url.startswith('http://'):
+            url = 'http://' + url
+        return url
 
-    
-#     def handleEncoding(self,response):
-#         if response.encoding == 'ISO-8859-1':
-#             charset_re = re.compile("((^|;)\s*charset=)([^\"]*)", re.M)
-#             charset=charset_re.search(response.content) 
-#             charset=charset and charset.group(3) or None 
-#             response.encoding = charset
-#         return response.encoding
-#         print response.encoding
-    
     #主爬虫类，处理
     def Spider(self,url):
-        
         if url.strip():
             try:
+                geturls = []
                 global htmlline
-                htmlline = requests.get(url.strip(),timeout = 5 )
+                htmlline = requests.get(self.http(url),timeout = 3 )
             except Exception,e:
-                print e
+                pass
             try:
                 if htmlline.text:
-                     
-#                      try:
-#                          htmlline.encoding = self.get_response_charset(htmlline.encoding)
-#                          #soup = BeautifulSoup(htmlline.content.decode(htmlline.encoding,'ignore'))
-#                      except UnicodeEncodeError,e:
-#                          #regex1 = r'<meta.+?charset=([-\w]+)'
-#                          regex_charset = r'((^|;)\s*charset=)([^\"]*)'
-#                          #regex_charset = r'<meta.*(?:(?:charset\s*=\s*["|\']?)|(?:charset.*content\s*=\s*["|\']\s*))([\d|\w|\-]+)[;|"|\'|\s]'
-#                          code = re.search(regex_charset,htmlline.content)
-#                          htmlline.encoding = code.group(1)
-#                          htmlline.encoding = self.get_response_charset(htmlline.encoding)
-#                          #soup = BeautifulSoup(htmlline.content.decode(htmlline.encoding,'ignore'))
-#                      except TypeError,e:
-#                          print 'chardet'
-#                          htmlline.encoding = chardet.detect(htmlline.content)['encoding']
-#                          htmlline.encoding = self.get_response_charset(htmlline.encoding)
-#                          #soup = BeautifulSoup(htmlline.content.decode(htmlline.encoding,'ignore'))
-#                      finally:
-                    #htmlline.encoding = self.get_response_charset(htmlline.encoding)
-                    soup = BeautifulSoup(htmlline.text,from_encoding=htmlline.encoding)#
+                    soup = BeautifulSoup(htmlline.text,from_encoding=htmlline.encoding)
                     links = soup.find_all('a',href=re.compile('^http|^/'))
                     regex = re.compile(r'(https?|ftp|mms):\/\/([A-z0-9]+[_\-]?[A-z0-9]+\.)*[A-z0-9]+\-?[A-z0-9]+\.[A-z]{2,}(\/.*)*\/?')  
-#                     href_link = []
+                href_link = []
                 for item in links:
-                    
                     if 'href' in str(item):
                         if regex.search(str(item)):
                             linkname = item.string
@@ -130,80 +97,59 @@ class Crawler(object):
                             if linkname is not None:  
                                 self.character_filter(linkname)
                                 self.character_filter(linkaddr)
-                                #if 'NoneType' in str(type(linkname)):
-                                
-                                #if self.md5(linkaddr) not in self.hashmap:                                   
-                                self.geturls.append(linkaddr)
-                                    #self.hashmap.append(self.md5(linkaddr))
-                                #self.href_link.append(linkaddr)
-                                #else: 
-                                    #href_link.append(linkname+':'+linkaddr+'\n')
-                                    #href_link.append(linkaddr)
-                return self.geturls        
-
+                                if 'NoneType' in str(type(linkname)):
+                                    href_link.append(linkaddr)
+                                else: 
+                                    href_link.append(linkaddr)
+                return href_link    
             except Exception,e:
                 print e
                 pass 
             
-            
+    #运行主爬虫类       
     def GetChildsByParent(self,parent):
         curr = self.Spider(parent)
         return curr
     
-    
-             
+    #返回当前层url
     def GetChilds(self,parents):
         ReturnValue = []
-        for i in range(len(parents)):
-            #print parents[i]
+        for i in parents:
             try:
-                ReturnValue.extend(self.GetChildsByParent(parents[i]))
-            except TypeError,e:
-                print e
+                ReturnValue.extend(self.GetChildsByParent(i))
+            except TypeError:
                 pass
-        print ReturnValue
         return ReturnValue
     
+    #去重方法
+    def repeat(self,url):
+        if self.md5(url) not in self.hashmap:
+            self.hashmap.append(self.md5(url))
+            return True
+        else:
+            return False
     
+    #运行方法
     def start(self):
-        #global self.url
+
+        self.currenturls.append(self.url)    #currenturls是个数组
         while self.currentdepth <= self.depth:
-            #for i in self.GetChilds(self.url):
-            self.url = self.GetChilds(self.url)
-            self.result.extend(self.url)
-            self.currentdepth += 1
-            
-            self.database.insert_data(self.result)
-        print len(self.result)
-        
-        self.conn_close()
-             
-    def depth_spider(self):
-        while self.currentdepth <= self.depth:
-            if len(self.href_link) == 0:
-                self.href_link.append(self.url)
             print '\nCurrent depth is: %d \n' % self.currentdepth
-            #print len(self.href_link)
-            for i in self.href_link:
-                #print i
-                self.Spider(i)
+            self.currenturls = self.GetChilds(self.currenturls)
+            for i in self.currenturls:
+                if self.repeat(i):
+                    self.result.append(i)
             self.currentdepth += 1
-            self.href_link = []
-            self.href_link.extend(self.geturls)
-            #print len(self.href_link)
-        self.database.insert_data(self.geturls)
+        self.database.insert_data(self.result)
         self.conn_close()
-        #print self.count                     
-                    
+          
+#数据库类              
 class DataBase(object):
-    
     conn = None
+    
+    #创建数据库表
     def __init__(self,dbfile):
-#         if os.path.isfile(dbfile):
-#             os.remove(dbfile)
-
         if self.conn is None: 
-
             self.conn = sqlite3.connect(dbfile)
             self.conn.text_factory = str
             self.cmd = self.conn.cursor()
@@ -214,9 +160,9 @@ class DataBase(object):
                     )
                 ''')
             self.conn.commit()
-        
+    
+    #插入数据
     def insert_data(self,href_link): 
-        
         for i in range(len(href_link)):
             url = href_link[i]
             if url is not None:
@@ -224,58 +170,15 @@ class DataBase(object):
                     self.cmd.execute("insert into Crawler (url) values(?)",(url,))
                 except Exception,e:
                      print e
-#         if self.count > 10000:
-#             print "=================="
-#             print self.count
-#             print "=================="
-#             self.conn.commit()
-#             self.count = 0
-        
+    #数据库关闭
     def conn_stop(self):
-        self.conn.commit()
-        #去重语句
-        self.cmd.execute('''
-            delete from Crawler where url not in 
-            (select distinct url from Crawler group by id )
-            ''')
         self.conn.commit()
         self.conn.close()
     
     
 
 start = time.clock()
-# Spider = Crawler(url,'J:/sql.db')
-# Spider.Spider()
-# # class Save(DataBase):
-# #     def __init__(self):
-# #         DataBase.conn_stop(self)
-# #         self.database.conn_stop()
-# def main():
-#     for i in range(10):
-#         t = ThreadClass(urlQueue)
-#         t.setDaemon(True)
-#         t.start()
-    
-
-
-
-
-Spider = Crawler('J:/sql.db',2,['http://www.creasyn.com/']) 
-'''
-In order to enhance the speed, the write 
-the class url handler class alone method, the 
-cycle so you do not need to repeatedly 
-call the main method of handling database 
-'''
-# with open('J:/1.txt','r') as file_object:
-# 
-#     for url in file_object:
-#         print url
+Spider = Crawler('J:/sql.db',2,'www.sina.com.cn') 
 Spider.start()
-#     save = Save()
-#     save()
-        
-        
-    
 end = time.clock()
 print end-start
